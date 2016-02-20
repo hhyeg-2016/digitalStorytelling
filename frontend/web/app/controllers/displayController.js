ca1a5d009ee3d6b2c1b001aa904d98ae36734a26
jihwan <jihwan@ualberta.ca>
2016-02-20 14:09:32 -0700

    changed some displaycontroller and display html

--- frontend/web/app/controllers/displayController.js
+++ frontend/web/app/controllers/displayController.js
@@ -1,9 +1,9 @@
 angular.module('dStory')
 .controller('displayController', ['$scope',
 function($scope) {
-  var profilePicture = "static/img/elderly-man-smile.jpg";
+  $scope.profilePicture = "http://localhost:5000/static/img/elderly-man-smile.jpg";
 
-  var data = [
+  $scope.data = [
     {
       type: text,
       content: 'Morbi faucibus tortor ut mattis ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse.'
