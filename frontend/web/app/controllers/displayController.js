aebac31a08d57303b1f12f6121f818aa83822801
Steven Myers <slmyers@ualberta.ca>
2016-02-20 13:32:19 -0700

    commit before pull

--- frontend/web/app/controllers/displayController.js
+++ frontend/web/app/controllers/displayController.js
@@ -1,16 +1,35 @@
 angular.module('dStory')
 .controller('displayController', ['$scope',
 function($scope) {
-  var profilePicture = "static/img/elderly-man-smile.jpg";
+  var $scope.profilePicture = "static/img/elderly-man-smile.jpg";
 
-  var data = [
+  var $scope.data = [
     {
-      type: text,
+      type: 'text',
       content: 'Morbi faucibus tortor ut mattis ultrices. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse.'
+      createdAt: '2016/11/14'
     },
     {
-      type: picture,
-      content: ''
+      type: 'picture',
+      content: 'web/static/img/elderly-man-smile.jpg',
+      createdAt: '2016/11/20'
     }
   ];
+
+  var $scope.isPicture = function(datum) {
+    if(datum.type === 'picture'){
+      return true;
+    }
+    return false;
+  };
+
+  var $scope.isText = function(datum) {
+    if(datum.type === 'text'){
+      return true;
+    }
+    return false;
+  };
+
+
+
 }]);
