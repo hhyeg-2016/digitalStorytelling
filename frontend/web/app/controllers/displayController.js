7389d8cf4f847d22a2856a36e9ccad14313d5c28
Steven Myers <slmyers@ualberta.ca>
2016-02-20 23:58:50 -0700

    autoplay

--- frontend/web/app/controllers/displayController.js
+++ frontend/web/app/controllers/displayController.js
@@ -1,6 +1,6 @@
 angular.module('dStory')
-.controller('displayController', ['$scope', 'storyService', '$stateParams',
-function($scope, storyService,$stateParams) {
+.controller('displayController', ['$scope', 'storyService', '$stateParams', '$interval', '$state',
+function($scope, storyService,$stateParams, $interval, $state) {
   console.log('displayController');
   console.log($stateParams.id);
   $scope.index = $stateParams.id;
@@ -8,6 +8,8 @@ function($scope, storyService,$stateParams) {
   $scope.story = $scope.stories[$scope.index];
   console.log($scope.story);
   $scope.content = $scope.story.content;
+  $scope.contentIndex = 0;
+  $scope.currContent = $scope.content[$scope.contentIndex];
   console.log($scope.content);
 
   $scope.isPicture = function(content) {
@@ -23,6 +25,21 @@ function($scope, storyService,$stateParams) {
     }
     return false;
   };
+
+  $scope.callAtInterval = function() {
+    console.log("$scope.callAtInterval - Interval occurred");
+    if ($scope.contentIndex < $scope.content.length - 1){
+      $scope.contentIndex++;
+      $scope.currContent = $scope.content[$scope.contentIndex];
+    } else {
+      $interval.cancel(interval);
+      $state.go('home');
+    }
+  };
+
+  var interval = $interval(function() {
+    $scope.callAtInterval();
+  }, 3000);
 
 
 }]);
