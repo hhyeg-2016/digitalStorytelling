ad1246b9b8c2d6b6d79881c8597dae445830b475
theresa-cy-ngo <theresa.cy.ngo@gmail.com>
2016-02-20 20:50:51 -0700

    Remove main.html and hardcode header to all pages

--- frontend/web/app/routes.js
+++ frontend/web/app/routes.js
@@ -3,26 +3,22 @@ angular.module('dStory')
             $locationProvider.html5Mode(true);
             $urlRouterProvider.otherwise('/home');
             $stateProvider
-              .state('main', {
-                abstract: 'true',
-                templateUrl: '/app/views/main.html'
-              })
-              .state('main.display', {
+              .state('display', {
                 url: '/display',
                 controller: 'displayController',
                 templateUrl: '/app/views/display.html'
               })
-              .state('main.submit', {
+              .state('submit', {
                 url: '/submit',
                 controller: 'submitController',
                 templateUrl: '/app/views/submit.html'
               })
-                .state('main.submitStory', {
+                .state('submitStory', {
                     url: '/submitStory',
                     controller: 'submitController',
                     templateUrl: '/app/views/submitStory.html'
                 })
-              .state('main.home', {
+              .state('home', {
                 url: '/home',
                 controller: 'homeController',
                 templateUrl: '/app/views/home.html'
