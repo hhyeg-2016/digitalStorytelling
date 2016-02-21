d3d5f856e8c0a091e1c43021053e9ffea195d764
ConnorSheremeta <thecon21@gmail.com>
2016-02-21 10:26:34 -0700

    colors

--- frontend/web/app/module.js
+++ frontend/web/app/module.js
@@ -1,6 +1,6 @@
 var app = angular.module('dStory', ['ngMaterial', 'ui.router'])
           .config(function($mdThemingProvider) {
             $mdThemingProvider.theme('default')
-              .primaryPalette('green')
-              .accentPalette('brown');
+                .primaryPalette('blue')
+                .accentPalette('light-blue');
           });
