1adfa5511fc45a9673411ef7c33c571f253eeb11
Steven Myers <slmyers@ualberta.ca>
2016-02-20 19:38:30 -0700

    modified home

--- frontend/web/app/controllers/homeController.js
+++ frontend/web/app/controllers/homeController.js
@@ -1,3 +1,27 @@
 angular.module('dStory')
 .controller('homeController', ['$scope', 'storyService', function(scope, storyService) {
+  $scope.stories = storyService.stories;
+  $scope.index = 0;
+  $scope.currStory = $scope.stories[$scope.index];
+
+  $scope.nextStory = function() {
+    if($scope.index < $scope.stories.length - 1){
+      $scope.index++;
+    } else if($scope.index === $scope.stories.length - 1) {
+      $scope.index = 0;
+    } else {
+      console.log('scope index out of bounds nextStory');
+    }
+  };
+
+  $scope.prevStory = function() {
+    if($scope.index === 0){
+      $scope.index = $scope.stories.length - 1;
+    } else if ($scope.index > 0 && $scope.index < $scope.stories.length - 1){
+      $scope.index--;
+    } else {
+      console.log('scope index out of bounds prevStory');
+    }
+  };
+  
 }]);
